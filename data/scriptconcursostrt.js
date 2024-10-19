18e1a562436377f2a3f8f83cf0512c4b25551c26
BuzzeeeD <h3online@gmail.com>
2024-10-18 21:57:57 -0300

    Update scriptconcursostrt.js

--- data/scriptconcursostrt.js
+++ data/scriptconcursostrt.js
@@ -9,7 +9,7 @@ let chatAberto = false; // Controle de estado do chat
 let questaoSelecionada = null; // Armazena a questão atual 
 // Função para carregar a planilha ao abrir a página
 function loadExcel() {
-    fetch('dados/TRT/data.xlsx')
+    fetch('data/dados/TRT/data.XLSX')
         .then(response => response.arrayBuffer())
         .then(data => {
             const workbook = XLSX.read(data, { type: 'array' });
